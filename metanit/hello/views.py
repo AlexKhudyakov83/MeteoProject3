aee36efdacc049867988f74d508fcd7a48625392
AlexKhudyakov83 <130558294+AlexKhudyakov83@users.noreply.github.com>
2023-09-24 18:21:04 +0300

    Продолжаем штудировать)

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Metanit.com")
+    return HttpResponse("<h2>Главная</h2>")
+
+def about(request):
+    return HttpResponse("<h3>О сайте</h3>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Контакты</h1>")
 
 
